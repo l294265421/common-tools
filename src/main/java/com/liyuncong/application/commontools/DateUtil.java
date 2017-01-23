54a4416d83fade50a3cb3bbadcec9ec29397eb6c
liyuncong <294265421@qq.com>
2017-01-23 14:56:02 +0800

    重构日期工具类

--- src/main/java/com/liyuncong/application/commontools/DateUtil.java
+++ src/main/java/com/liyuncong/application/commontools/DateUtil.java
@@ -5,10 +5,11 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import org.apache.commons.lang3.time.DateFormatUtils;
-
 public class DateUtil {
-	public static String yyyyMMddHHmmss1 = "yyyy-MM-dd HH:mm:ss";
+	public static final String YYYY_MM_DD_HH_MM_SS_ONE = "yyyy-MM-dd HH:mm:ss";
+	
+	private DateUtil() {
+	}
 	
 	public static Date toDate(String date, String format) throws ParseException {
 		SimpleDateFormat formatter = new SimpleDateFormat(format);
@@ -24,18 +25,13 @@ public class DateUtil {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
 		calendar.add(Calendar.MONTH, -1);    //得到前一个月 
-		Date monthAgo = calendar.getTime();
-		return monthAgo;
+		return calendar.getTime();
 	}
 	
 	public static boolean date1AfterDate2(Date date1, Date date2) {
 		long long1 = date1.getTime();
 		long long2 = date2.getTime();
-		if (long1 > long2) {
-			return true;
-		} else {
-			return false;
-		}
+		return long1 > long2;
 	}
 	
 	public static long date1MinusDate2(Date date1, Date date2) {
